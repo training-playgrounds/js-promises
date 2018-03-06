6b88d573f4b44ff50b5ef1bf44c71bdc72c23c63
jalbr74 <jalbr74@gmail.com>
2018-03-06 06:37:05 -0700

    Initial checkin

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: "./src/main",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "app.js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /.ts$/,
+                loader: "ts-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            chunksSortMode: 'dependency',
+            inject: true,
+            livereload: true
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets',
+                to: 'assets'
+            }
+        ])
+    ]
+};
